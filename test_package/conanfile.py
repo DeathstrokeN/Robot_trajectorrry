2bf2b4c6c00f8f5596e699dfd988550f1fc51790
Benjamin Navarro <navarro.benjamin13@gmail.com>
2020-07-21 16:57:08 +0200

    ci: add Gitlab CI script

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,7 +3,7 @@ import os
 from conans import ConanFile, CMake, tools
 
 
-class UrdfdomcppTestConan(ConanFile):
+class ProjectTemplateTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
