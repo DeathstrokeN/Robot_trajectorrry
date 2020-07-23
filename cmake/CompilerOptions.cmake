772ff7df7a23750de8271569ffa0c685f7850c09
Benjamin Navarro <navarro.benjamin13@gmail.com>
2020-07-23 16:17:49 +0200

    fix(compilation): incorrect setting of compiler options

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -3,18 +3,21 @@ option(WARNINGS_AS_ERRORS "Treat warnings as errors" ON)
 
 function(add_warnings target)
     if(MORE_WARNINGS)
-        set(MSVC_WARNINGS "/W4")
-        set(GCC_WARNINGS "-Wall -Wextra")
+        set(MSVC_WARNINGS /W4)
+        set(GCC_WARNINGS -Wall -Wextra)
+    else()
+        set(MSVC_WARNINGS)
+        set(GCC_WARNINGS)
     endif()
     
     if(WARNINGS_AS_ERRORS)
-        set(MSVC_WARNINGS "${MSVC_WARNINGS} /WX")
-        set(GCC_WARNINGS "${GCC_WARNINGS} -Werror")
+        set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
+        set(GCC_WARNINGS ${GCC_WARNINGS} -Werror)
     endif()
 
     target_compile_options(
         ${target} PRIVATE
-        $<$<CXX_COMPILER_ID:MSVC>:/W4 /WX>
-        $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wall -Wextra -Werror>
+        $<$<CXX_COMPILER_ID:MSVC>:${MSVC_WARNINGS}>
+        $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:${GCC_WARNINGS}>
     )
 endfunction(add_warnings)
