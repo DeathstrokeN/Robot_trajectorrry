be0e94d217b2d15fef57b9e276feca905062a4ee
Benjamin Navarro <navarro.benjamin13@gmail.com>
2021-09-09 16:57:08 +0200

    build: update conan remote

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -1,15 +1,15 @@
 # Automatically downloads conan.cmake if not already present
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD   https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake
+    file(DOWNLOAD   https://github.com/conan-io/cmake-conan/raw/v0.16.1/conan.cmake
                     ${CMAKE_BINARY_DIR}/conan.cmake)
 endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
-  
+
 conan_add_remote(
     NAME bnavarro
-    URL https://bnavarro.jfrog.io/artifactory/api/conan/bnavarro 
+    URL https://navarrob.jfrog.io/artifactory/api/conan/navarrob-conan
 )
 
 if(ENABLE_TESTING)
